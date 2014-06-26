4c7f74020acb1cdabfab1af9eb6d59879afb7997
Sim Brar <sim@simbrar.com>
2014-06-26 16:03:56 -0700

    first readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnRailsTutorial::Application.config.secret_key_base = '71b886cbca4d9d2f27a442c82feab78645f19cf8dabc5974d1383f6ce95b615e379d1e68d774df30541b8e3682a95317289055cb24e4eb7b4cf25abb76933f5d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LearnRailsTutorial::Application.config.session_store :cookie_store, key: '_learn-rails-tutorial_session'
