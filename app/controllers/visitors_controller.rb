944cd248abdbb628b49227326659a708e5b6eba4
Sim Brar <sim@simbrar.com>
2014-06-27 16:00:25 -0700

    fixed turbolinks

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,5 @@ class VisitorsController < ApplicationController
 	def new
 		#Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
-		flash.now[:notice] = 'Welcome!'
-		flash.now[:notice] = 'My birthday is soon.'
-		render 'visitors/new'
 	end
 end
