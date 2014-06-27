ec995347e99af50fcdaf0e6063b41bc80fd08371
Sim Brar <sim@simbrar.com>
2014-06-27 14:51:22 -0700

    added layout files

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,9 @@
 class VisitorsController < ApplicationController
 	def new
+		#Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:notice] = 'My birthday is soon.'
 		render 'visitors/new'
 	end
 end
